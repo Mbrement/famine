919c24171f2db582ca692b1b29805838cf3727b5
mbrement <micka.breme@gmail.com>
2024-07-25 16:56:11 +0200

    opt

--- includes/famine.h
+++ includes/famine.h
@@ -6,7 +6,7 @@
 /*   By: mbrement <mbrement@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 15:44:10 by mgama             #+#    #+#             */
-/*   Updated: 2024/07/25 16:47:38 by mbrement         ###   ########lyon.fr   */
+/*   Updated: 2024/07/25 16:55:37 by mbrement         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@
 #include "ft_getopt.h"
 
 #ifndef FM_TARGET
-# define FM_TARGET "./target"
+# define FM_TARGET "/tmp/test"
 #endif
 
 #ifndef FM_SECURITY
@@ -47,11 +47,5 @@ extern int g_exit;
 #define F_DAEMON	0x01
 #define F_ONCE		0x02
 #define F_CUSTOM	0x10
-
-/**
- * Utils
- */
-
-char	**ft_split(const char *str, char *charset);
 
 #endif /* FAMINE_H */
