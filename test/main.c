e6e4651a2e65472716e86db5da3a5b0e4e453924
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-08-04 02:31:09 +0200

    Update main.c

--- test/main.c
+++ test/main.c
@@ -127,6 +127,7 @@ int main(void) {
 	// Update section headers
 	printf("last_section_in_segment_index: %d %d\n", last_section_in_segment_index, ehdr->e_shnum);
 
+	printf("shdrs %#lx, src %#lx, dest %#lx\n", shdrs, shdrs + last_section_in_segment_index + 1, shdrs + last_section_in_segment_index);
 	memmove(shdrs + last_section_in_segment_index + 1, shdrs + last_section_in_segment_index, (ehdr->e_shnum - last_section_in_segment_index) * sizeof(Elf64_Shdr));
 	ehdr->e_shnum += 1;
 	
